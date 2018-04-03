8968b266e8104a96e7cd313a6f7d4970fdd0ab6c
Martin-Develop <martin.neumann1@hs-augsburg.de>
2018-04-03 13:45:56 +0200

    Squirrel

---
+++ strategy game/src/HandOperatedMasterSquirrel.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+
+public class HandOperatedMasterSquirrel extends MasterSquirrel {
+
+    public static void catchOperation() throws IOException {
+
+        while(true){
+            char op = (char)System.in.read();
+            System.in.read(new byte[System.in.available()]);
+
+            switch (op){
+                case '4':
+                    setOperation("LEFT");
+                    return;
+                case '6':
+                    setOperation("RIGHT");
+                    return;
+                case '8':
+                    setOperation("TOP");
+                    return;
+                case '2':
+                    setOperation("DOWN");
+                    return;
+                default:
+                    System.out.println("Keine Richtung ausgewählt!\nBitte erneut auswählen.");
+                    break;
+            }
+        }
+
+
+    }
+
+    private static void setOperation(String direction){
+        System.out.println(direction);
+    }
+
+}
---
+++ strategy game/src/MasterSquirrel.java
@@ -0,0 +1,32 @@
+public class MasterSquirrel extends Entity{
+/*
+    public int energie = 100;
+
+    public boolean checkSquirrel(Entity entity){
+        return entity instanceof MiniSquirrel;
+    }
+
+    public void setEnergie(int energie){
+        this.energie = energie;
+    }
+    public int getEnergie(){
+        return energie;
+    }
+
+    public void sendEnergie(Entity entity, int energie){
+        if(checkSquirrel(entity)){
+            MiniSquirrel miniSquirrel = (MiniSquirrel) entity;
+
+            //(miniSquirrel.getEnergie()+ energie>=100)?MiniSquirrel.setEnergie(100):MiniSquirrel.setEnergie(MiniSquirrel.energie + energie);
+
+            if(miniSquirrel.getEnergie()+ energie>=100){
+                MiniSquirrel.setEnergie(100);
+            }
+            else{
+                MiniSquirrel.setEnergie(MiniSquirrel.energie + energie);
+            }
+        }
+    }*/
+}
+
+

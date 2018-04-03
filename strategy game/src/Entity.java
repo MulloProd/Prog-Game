c99cb000fea056f4ec93465073602adad12dc627
schafkoepfer <38036591+schafkoepfer@users.noreply.github.com>
2018-04-03 15:07:20 +0200

    Beasts Bewegung

--- strategy game/src/Entity.java
+++ strategy game/src/Entity.java
@@ -1,8 +1,8 @@
 public abstract class Entity {
-    private final int ID;
-    private int energy;
-    private final int startEnergy;
-    private XY position;
+    protected final int ID;
+    protected int energy;
+    protected final int startEnergy;
+    protected XY position;
 
     public Entity(int id, int energy, XY position) {
         ID = id;
@@ -27,6 +27,10 @@ public abstract class Entity {
 
     public void UpdateEnergy(int value){
         energy += value;
+    }
+
+    public String toString(){
+        return "Pos: " + position.X + ", " + position.Y + "; Energy: " + energy + "/" + startEnergy;
     }
 
 }
--- strategy game/src/XY.java
+++ strategy game/src/XY.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public final class XY {
     public final int X;
     public final int Y;
@@ -9,5 +11,10 @@ public final class XY {
 
     public XY ADD(XY vector){
         return new XY(X+vector.X, Y+vector.Y);
+    }
+
+    public static XY RandomVector(){
+        XY temp = new XY(new Random().nextInt(1)-1, new Random().nextInt(1)-1);
+        return temp;
     }
 }
